5217df34a3115d5e0feb42490cf25ea37c035542
Lindsey Bowen <lindsbow102@yahoo.com>
2018-06-20 13:03:46 -0700

    basic redux playground

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,1 +1,43 @@
-console.log('101');
+import { createStore } from "redux";
+
+// Setting state to a default value
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+
+console.log(store.getState());
+
+// Actions -- object that gets sent to the store
+// e.g. increment the count
+store.dispatch({
+  type: "INCREMENT"
+});
+
+store.dispatch({
+  type: "INCREMENT"
+});
+
+store.dispatch({
+  type: "RESET"
+});
+
+store.dispatch({
+  type: "DECREMENT"
+});
+
+console.log(store.getState());
