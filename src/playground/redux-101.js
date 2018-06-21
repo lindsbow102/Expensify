00a444ee8ac10a2981f08383d908b52204974b4e
Lindsey Bowen <lindsbow102@yahoo.com>
2018-06-21 13:53:42 -0700

    multiple reducers

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -22,28 +22,34 @@ const resetCount = () => ({
 })
 
 // Setting state to a default value
-const store = createStore((state = { count: 0 }, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        count: state.count + action.incrementBy
-      };
-    case "DECREMENT":
-      return {
-        count: state.count - action.decrementBy
-      };
-    case "SET":
-      return {
-        count: action.count
-      };
-    case "RESET":
-      return {
-        count: 0
-      };
-    default:
-      return state;
-  }
-});
+//This function is called a reducer
+//REDUCERS
+//1. Reducers are PURE functions (input/out are all inside reducer scope--no outside variables or data)
+//2. Never change state or action
+const countReducer = (state = { count: 0 }, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return {
+          count: state.count + action.incrementBy
+        };
+      case "DECREMENT":
+        return {
+          count: state.count - action.decrementBy
+        };
+      case "SET":
+        return {
+          count: action.count
+        };
+      case "RESET":
+        return {
+          count: 0
+        };
+      default:
+        return state;
+    };
+};
+
+const store = createStore(countReducer);
 
 // Watch for changes to the redux store state
 const unsubscribe = store.subscribe(() => {
