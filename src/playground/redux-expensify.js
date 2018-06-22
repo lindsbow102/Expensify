4b76da0faf3550265b497ec3fd6fee6b66ab56f5
Lindsey Bowen <lindsbow102@yahoo.com>
2018-06-22 11:18:05 -0700

    complete redux store

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -19,27 +19,62 @@ const addExpense = ({
 });
 //REMOVE_EXPENSE
 const removeExpense = ({ id } = {}) => ({
-    type: 'REMOVE_EXPENSE',
-    id
-})
+  type: "REMOVE_EXPENSE",
+  id
+});
 //EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: "EDIT_EXPENSE",
+  id,
+  updates
+});
 //SET_TEXT_FILTER
+const setTextFilter = (text = "") => ({
+  type: "SET_TEXT_FILTER",
+  text
+});
+
 //SORT_BY_DATE
+const sortByDate = () => ({
+  type: "SORT_BY_DATE"
+});
+
 //SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: "SORT_BY_AMOUNT"
+});
+
 //SET_START_DATE
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
 //SET_END_DATE
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
 
 //Expenses Reducer
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
-    case 'ADD_EXPENSE':
-        return [
-            ...state,
-            action.expense
-        ];
-    case 'REMOVE_EXPENSE':
-       return state.filter(({ id }) => id !== action.id); //if not equal, this will return true, and item will be kept        
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
+    case "REMOVE_EXPENSE":
+      return state.filter(({ id }) => id !== action.id); //if not equal, this will return true, and item will be kept
+    case "EDIT_EXPENSE":
+      return state.map(expense => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        }
+      });
     default:
       return state;
   }
@@ -54,9 +89,34 @@ const filtersReducerDefaultState = {
 };
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case "SET_TEXT_FILTER":
+      return {
+        ...state,
+        text: action.text
+      };
+    case "SORT_BY_DATE":
+      return {
+        ...state,
+        sortBy: "date"
+      };
+    case "SORT_BY_AMOUNT":
+      return {
+        ...state,
+        sortBy: "amount"
+      };
+    case "SET_START_DATE":
+      return {
+          ...state,
+          startDate: action.startDate
+      };
+    case "SET_END_DATE":
+      return {
+          ...state,
+          endDate: action.endDate
+      };
     default:
       return state;
-  }
+  };
 };
 
 // Store Creation
@@ -68,23 +128,36 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState());
+  console.log(store.getState());
 });
 
-const expenseOne = store.dispatch(addExpense({
-    description: 'Rent',
-    amount: 100
-}));
+// const expenseOne = store.dispatch(
+//   addExpense({
+//     description: "Rent",
+//     amount: 100
+//   })
+// );
+// //console.log(expenseOne);
+// const expenseTwo = store.dispatch(
+//   addExpense({
+//     description: "Coffee",
+//     amount: 300
+//   })
+// );
 
-//console.log(expenseOne);
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
-const expenseTwo = store.dispatch(addExpense({
-    description: 'Coffee',
-    amount: 300
-}));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+// store.dispatch(setTextFilter("Coffee"));
+// store.dispatch(setTextFilter());
 
+// store.dispatch(sortByAmount()); // amount
+// store.dispatch(sortByDate()); // date
+
+store.dispatch(setStartDate(125)); // start date 125
+store.dispatch(setStartDate());  // start date undefined
+store.dispatch(setEndDate(1250));  // end date 1250
 
 const demoState = {
   expenses: [
