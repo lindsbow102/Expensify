8b4a492e678abd467dcb40f81ab5c625c8a3ab24
Lindsey Bowen <lindsbow102@yahoo.com>
2018-06-25 13:18:24 -0700

    'Create Expense' page now working

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,96 @@
+import React from "react";
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+
+// If no argument is passed into moment, it represents now (the current point in time).
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
+console.log(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+class ExpenseForm extends React.Component {
+  state = {
+    description: "",
+    note: "",
+    amount: "",
+    createdAt: moment(),
+    calendarFocused: false,
+    error: ''
+  };
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+  };
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }));
+  };
+  onAmountChange = (e) => {
+    const amount = e.target.value;
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+        this.setState(() => ({ amount })); // User can now delete since we provided "!amount ||"
+    }    
+  };
+  onDateChange = (createdAt) => {
+      if(createdAt) {
+        this.setState(() => ({ createdAt })); // Prevents user from being able to clear the date value
+      }    
+  };
+  onFocusChange = ({ focused }) => {
+      this.setState(() => ({ calendarFocused: focused }));
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.description || !this.state.amount) {
+        this.setState(() => ({
+            error: 'Please Provide Description and Amount.'
+        }))        
+    } else {
+        this.setState(() => ({ error: '' }));
+        this.props.onSubmit({
+            description: this.state.description,
+            amount: parseFloat(this.state.amount, 10) * 100,
+            createdAt: this.state.createdAt.valueOf(),
+            note: this.state.note
+        });
+    }
+  };
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
+          <input 
+            type="text" 
+            placeholder="Description" 
+            autoFocus 
+            value={this.state.description}
+            onChange={this.onDescriptionChange} />
+          <input 
+            type="text" 
+            placeholder="Amount"
+            value={this.state.amount}
+            onChange={this.onAmountChange} />
+        <SingleDatePicker
+            date={this.state.createdAt} // momentPropTypes.momentObj or null
+            onDateChange={this.onDateChange} // PropTypes.func.isRequired
+            focused={this.state.calendarFocused} // PropTypes.bool
+            onFocusChange={this.onFocusChange} // PropTypes.func.isRequired
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+        />
+        <textarea 
+            placeholder="Add a note for your expense (optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange} 
+        />
+        <button>Add Expense</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ExpenseForm;
