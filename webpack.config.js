85c6395b1e22e22eed882fa37d4c4a057ef4f633
Lindsey Bowen <lindsbow102@yahoo.com>
2018-07-05 13:23:08 -0600

    More production webpack build setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 //entry (will be app.js) -> output
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 // console.log(__dirname);
 //console.log(path.join(__dirname, 'public'));
 
-module.exports = (env) => {
-    const isProduction = env === 'production';
-    //console.log('env', env);
+module.exports = env => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
+  //console.log('env', env);
   return {
     entry: "./src/app.js",
     output: {
@@ -23,13 +25,29 @@ module.exports = (env) => {
         },
         {
           test: /\.s?css$/,
-          use: ["style-loader", "css-loader", "sass-loader"]
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
         }
       ]
     },
+    plugins: [CSSExtract],
     // Source mapping based on whether we are using development or production mode
     // 'Source-map' is an external file
-    devtool: isProduction ?  "source-map" : "cheap-module-eval-source-map",
+    devtool: isProduction ? "source-map" : "inline-source-map",
     //so we can do yarn run dev-server in CLI
     devServer: {
       contentBase: path.join(__dirname, "public"),
