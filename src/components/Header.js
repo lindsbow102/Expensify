d84db019cf9811403a642764ba8badb076726322
Lindsey Bowen <lindsbow102@yahoo.com>
2018-07-18 17:22:04 -0600

    Setup basic firebase user login with google

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header>
       <h1>Expensify</h1>
-      <NavLink to='/' activeClassName='is-active' exact={true}>Dashboard</NavLink>
+      <NavLink to='/dashboard' activeClassName='is-active'>Dashboard</NavLink>
       <NavLink to='/create' activeClassName='is-active'>Create Expense</NavLink>
     </header>
   ); 
