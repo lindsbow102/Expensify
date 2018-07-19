efb91ed6442aed207563d6715bf2b6421e6ca078
Lindsey Bowen <lindsbow102@yahoo.com>
2018-07-19 10:02:08 -0600

    Wired up user logout

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 // Test does Header render
 test('should render Header correctly', () => {
-    const wrapper = shallow(<Header />);
+    let wrapper = shallow(<Header startLogout={() => { }}/>);
     expect(wrapper).toMatchSnapshot();
+});
+
+// Testing Logout button
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    let wrapper = shallow(<Header startLogout={startLogout}/>);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
 });
