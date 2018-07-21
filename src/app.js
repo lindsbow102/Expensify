b54cfb7ce276442cfab003696a67cb5312a02ceb
Lindsey Bowen <lindsbow102@yahoo.com>
2018-07-20 18:25:08 -0600

    auth reducer and tests

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { startSetExpenses } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
@@ -30,6 +30,8 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+    //console.log('uid', user.uid);
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
@@ -37,6 +39,7 @@ firebase.auth().onAuthStateChanged((user) => {
       }
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,9 @@
-import { combineReducers, createStore, applyMiddleware, compose } from 'redux'; // Thunk allows redux to dispatch functions
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux'; 
+import thunk from 'redux-thunk';  // Thunk allows redux to dispatch functions
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
-import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
+
 
 // If we are using DEVTOOLS, we want to make sure it is correctly set up.. if not, property doesn't exist
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -12,7 +14,8 @@ export default () => {
     const store = createStore(
         combineReducers({
           expenses: expensesReducer,
-          filters: filtersReducer
+          filters: filtersReducer,
+          auth: authReducer
         }),
         composeEnhancers(applyMiddleware(thunk))
       );
